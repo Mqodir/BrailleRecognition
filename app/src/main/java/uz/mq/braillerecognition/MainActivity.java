42e61b9e59caad5c730e28bfc1336932a2f31dbe
Muhammadqodir <MQAbduvoitov@users.noreply.github.com>
2020-12-04 19:15:57 +0500

    Nav Menu & Main Menu

--- app/src/main/java/uz/mq/braillerecognition/MainActivity.java
+++ app/src/main/java/uz/mq/braillerecognition/MainActivity.java
@@ -1,6 +1,8 @@
 package uz.mq.braillerecognition;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.view.ViewOutlineProvider;
@@ -10,6 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
 
     AppBarLayout appBarLayout;
+    DrawerLayout drawer;
     ViewOutlineProvider outlineProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +37,9 @@ public class MainActivity extends AppCompatActivity {
         setActionBar();
         setTitle("");
         setActionBarShadow(false);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
+
 
     }
 
@@ -71,5 +70,16 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawer.openDrawer(Gravity.LEFT);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
